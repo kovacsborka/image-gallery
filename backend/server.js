5c23cfac7cef9634be1d3200e489c1d0c9695318
kovacsborka <kovacsborbalaterez@gmail.com>
2022-04-22 08:45:13 +0200

    delete missing

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const path = require("path");
+const fileUpload = require("express-fileupload");
+const fs = require("fs");
 
 const app = express();
 
 const pathToFrontend = path.join(`${__dirname}/../frontend`)
 const port = 9000
 
+const dataLocation = path.join(`${__dirname}/../frontend/`);
 
 
 // function getFunction(request, response){
 //     response.sendFile(`${pathToFrontend}/index.html`);
 // }
-
+app.use(fileUpload());
+app.use("/upload", express.static(`${__dirname}/../frontend/upload`));
 app.use(express.json());
 app.use("/pub", express.static(`${__dirname}/../frontend/public`));
 
@@ -21,6 +25,49 @@ app.get("/", (req, res) => {
 });
 
   
+
+let jsonData = [];
+try {
+    let data = fs.readFileSync(`${dataLocation}data.json`, error => {
+        if (error) {
+            console.log(error);
+        }
+    });
+    jsonData = JSON.parse(data);
+} catch (error) {
+    fs.writeFile(`${dataLocation}data.json`, JSON.stringify(jsonData), (error) => {
+        if (error) {
+            console.log(error);
+        }
+    });
+}
+
+const uploads = path.join(`${__dirname}/../frontend/public/images/`);
+
+app.post("/", (req, res) => {
+    // Upload image
+    const picture = req.files.filename
+    const answer = {};
+
+    if (picture) {
+        picture.mv(uploads + picture.name, error => {
+            return res.status(500).send(error);
+        });
+    }
+    answer.pictureName = picture.name;
+
+    // Upload data from form
+    const formData = req.body;
+    formData.filename = picture.name;
+    jsonData.push(formData);
+
+    fs.writeFile(`${dataLocation}data.json`, JSON.stringify(jsonData), (error) => {
+        if (error) {
+            console.log(error);
+        }
+    });
+    res.send(answer);
+});
 
 
 
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -19,12 +19,21 @@ const swiperSlideComponent = ({filename, title}) => {
         <div class="swiper-slide">
             <h2>${title}</h2>
             <img src="/pub/images/${filename}">
+            <button>Delete</button>
         </div>
-`
+        `;
 }
 
-
-
+const formComponent = () => {
+    return `
+        <form action="" id="form">
+            <input type="file" name="image">
+            <input type="text" placeholder="Title" name="title">
+            <input type="text" placeholder="Photographer" name="photographer">
+            <button>Submit</button>
+        </form>
+        `;
+}
 
 const loadEvent = async () => {
     console.log("loaded");
@@ -34,10 +43,51 @@ const loadEvent = async () => {
 
     rootElement.insertAdjacentHTML("beforeend", swiperComponent(result, swiperSlideComponent), )
 
+    rootElement.insertAdjacentHTML("beforeend", formComponent())
+
     const swiper = new Swiper(".swiper", {
         loop: true
     })
 
+
+
+
+
+    const formElement = document.getElementById("form");
+    formElement.addEventListener("submit", e => {
+        e.preventDefault();
+       /*  console.dir(e); */
+
+        const formData = new FormData() 
+        formData.append("title", e.target.querySelector(`input[name="title"]`).value) 
+        formData.append("photographer", e.target.querySelector(`input[name="photographer"]`).value) 
+        formData.append("filename", e.target.querySelector(`input[name="image"]`).files[0]) 
+        
+
+
+        console.log(formData);
+
+        
+        const fetchSettings = {
+            method: "POST",
+            body: formData
+        }
+
+        fetch("/", fetchSettings) 
+            .then(async data => {
+                if (data.status === 200) {
+                    const res = await data.json()
+                    // e.target.outerHTML = `<img src="upload/${res.pictureName}">`
+                    // console.dir(data);
+                }
+            })
+            .catch(error => {
+                e.target.outerHTML = "error";
+                console.dir(error)
+            })
+
+            
+    })
 
 
 }
